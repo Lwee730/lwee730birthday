a2d2b77a820e34ece37fd6e396f64b6d76151557
Lwee730 <qwaspl8@gmail.com>
2025-07-08 21:18:04 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,64 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// 触发播放
+const video = document.getElementById("main-video");
+const music = document.getElementById("bg-music");
+const button = document.getElementById("playButton");
+
+button.addEventListener("click", () => {
+  video.play().catch(() => {});
+  music.play().catch(() => {});
+  button.style.display = "none";
+});
+
+// 滚动动画
+gsap.to("#main-title", {
+  opacity: 0,
+  y: -50,
+  scrollTrigger: {
+    trigger: ".intro",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
+});
+
+gsap.to("#sub-title", {
+  opacity: 0,
+  y: 50,
+  scrollTrigger: {
+    trigger: ".intro",
+    start: "top top",
+    end: "bottom top",
+    scrub: true
+  }
+});
+
+gsap.from(".panel.image p", {
+  opacity: 0,
+  y: 40,
+  scrollTrigger: {
+    trigger: ".panel.image",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  }
+});
+
+gsap.from(".panel.video p", {
+  opacity: 0,
+  y: 40,
+  scrollTrigger: {
+    trigger: ".panel.video",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  }
+});
+
+gsap.from("#scroll-video", {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".panel.video",
+    start: "top 80%",
+    toggleActions: "play none none reverse"
+  }
+});
